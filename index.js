3be12712061f83296a003dfa6c5eaf39b336274f
Simmington <simmington96@gmail.com>
2025-02-08 20:15:56 +0000

    style buttons and text

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// 1. Create a function that returns a random choice of rock, paper 
-//  or scissors
-// 2. Create a function that accepts a player input of rock, paper
-//  scissors
-// 3. Create 2 variables humanScore and computerScore.
-// 4. Store a log the scores of each player.
-// 5. Implement the logic of which value trumps another
-// 6. Write the logic for a single round
-// 7. Write the logic for a full game (5 rounds)
 
 function getComputerChoice(max){
     return Math.floor(Math.random() * max); 
@@ -14,33 +5,57 @@ function getComputerChoice(max){
 
 function computerOutput(computerChoice){
     if (computerChoice === 0){
-    return "rock"}
+    return "Rock"}
 else if (computerChoice === 1){
-    return "paper"}
+    return "Paper"}
 else if (computerChoice === 2){
-    return "scissors"}
+    return "Scissors"}
 }
 
-function transform(getHumanChoice){
-    return getHumanChoice.toLowerCase()
-}
+const rockBtn = document.querySelector("#rockBtn")
+const paperBtn = document.querySelector("#paperBtn")
+const scissorsBtn = document.querySelector("#scissorsBtn")
+
+rockBtn.addEventListener("click", function() {
+    playRound("rock");
+});
+
+paperBtn.addEventListener("click", function() {
+    playRound("paper");
+});
+
+scissorsBtn.addEventListener("click", function() {
+    playRound("scissors");
+});
 
 let humanScore = 0
 let computerScore = 0
+
+function playRound(humanChoiceString){
+
+    let computerChoice = getComputerChoice(3);
+    let computerChoiceString = computerOutput(computerChoice);
+
+    let message = result(computerChoiceString, humanChoiceString);
+
+    document.querySelector("#result").textContent = `Computer chose: ${computerChoiceString}`;
+    document.querySelector("#score").textContent = `Score: You - ${humanScore}, Computer: ${computerScore}`
+
+}
 
 function result(computerChoiceString, humanChoiceString){
     if (computerChoiceString === humanChoiceString){
         return "Draw!"
     }
-    else if (computerChoiceString === "rock" && humanChoiceString === "scissors"){
+    else if (computerChoiceString === "Rock" && humanChoiceString === "scissors"){
         computerScore++;
     return "You lose!";
     }
-    else if (computerChoiceString === "paper" && humanChoiceString === "rock"){
+    else if (computerChoiceString === "Paper" && humanChoiceString === "rock"){
     computerScore++;
     return "You lose!";
     }
-    else if (computerChoiceString === "scissors" && humanChoiceString === "paper"){
+    else if (computerChoiceString === "Scissors" && humanChoiceString === "paper"){
     computerScore++;
     return "You lose!";
     }
@@ -50,26 +65,6 @@ function result(computerChoiceString, humanChoiceString){
     }
 }
 
-for (let round = 1; round <= 5; round++){
-    console.log(`\nRound: ${round}`)
 
-    let computerChoice = getComputerChoice(3)
-    let computerChoiceString = computerOutput(computerChoice)
 
-    let getHumanChoice = prompt("Please choose rock, paper or scissors.")
-    let humanChoiceString = transform(getHumanChoice)
 
-    console.log(`Computer chose: ${computerChoiceString}`)
-    console.log(`Your choice: ${humanChoiceString}`)    
-
-    console.log(result(computerChoiceString, humanChoiceString))
-    console.log(`Score: Human - ${humanScore}, Computer - ${computerScore}`)
-}
-
-if (humanScore > computerScore){
-    console.log("Congratulations! You win!")
-} else if (humanScore < computerScore){
-    console.log("You have been defeated!")
-} else {
-    console.log("It's a draw!")
-}
